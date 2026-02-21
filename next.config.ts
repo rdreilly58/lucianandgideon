49eaf0d073d6c32206a7a624303dc44c02056af5
Robert Reilly <robert.reilly@peraton.com>
2026-02-21 10:02:41 -0500

    Fix: Allow external images from supercoloring.com

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.supercoloring.com',
+        port: '',
+        pathname: '/sites/default/files/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
